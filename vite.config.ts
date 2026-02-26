0d97bf541a2a350260f0bcb37bc1800642b00f48
Evgeniy <Masquered@yandex.ru>
2026-02-26 19:08:46 +0500

    feat: add GitHub actions

--- vite.config.ts
+++ vite.config.ts
@@ -9,29 +9,26 @@ export default defineConfig({
   base: "/focus-timer/",
   plugins: [react(), tailwindcss(), svgr(), VitePWA({
       registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
       manifest: {
         name: "focus-timer",
         short_name: "focus-timer",
-        start_url: "/",
+        start_url: ".",
         display: "standalone",
         background_color: "#fff",
         theme_color: "#000",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
